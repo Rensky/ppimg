bd73656b591aa1c734c376c8a4d80a596bf11789
Rensky <rensky0742@gmail.com>
2015-12-22 23:08:29 +0800

    加入動態Canvas main img 區域

--- config.rb
+++ config.rb
@@ -1,16 +1,17 @@
 require 'compass/import-once/activate'
+Encoding.default_external = 'utf-8'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
 fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
